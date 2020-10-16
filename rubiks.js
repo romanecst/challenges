5c514e1d15ad61b3e9c3411c84cc25d0c9124f6e
romanecst <72913790+romanecst@users.noreply.github.com>
2020-10-16 21:46:25 +0200

    Delete rubiks.js

--- rubiks.js
+++
@@ -1,8 +0,0 @@
-for(var i=0; i<document.getElementsByClassName("square").length; i++){
-    var colors = ['blue', 'red', 'green','yellow','white','orange'];
-    var colorIndex = Math.round((colors.length-1)*Math.random()); 
-    var c = colors[colorIndex];
-    document.getElementsByClassName("square")[i].style.backgroundColor = `${c}`;
-}
-
-
